421c283a24533144023ef09a6896c6c134068adc
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-03-18 01:32:19 -0300

    [MIG] fleet_vehicle_license_plate_alt: Migration to 14.0

--- fleet_vehicle_license_plate_alt/tests/test_vehicle_license_plate_alt.py
+++ fleet_vehicle_license_plate_alt/tests/test_vehicle_license_plate_alt.py
@@ -14,7 +14,7 @@ class TestVehicleLicensePlatAlt(TransactionCase):
                 "color": "Black",
                 "location": "Grand-Rosiere",
                 "doors": 5,
-                "driver_id": self.env.ref("base.user_demo_res_partner").id,
+                "driver_id": self.env.ref("base.partner_demo").id,
                 "odometer_unit": "kilometers",
                 "car_value": 20000,
                 "model_id": self.env.ref("fleet.model_astra").id,
