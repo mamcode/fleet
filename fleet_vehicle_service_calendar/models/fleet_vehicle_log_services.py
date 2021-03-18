4a4492ff2790d96da2ebaf052b64848e528d8423
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-03-18 19:05:00 -0300

    [MIG] fleet_vehicle_service_calendar: Migration to 14.0

--- fleet_vehicle_service_calendar/models/fleet_vehicle_log_services.py
+++ fleet_vehicle_service_calendar/models/fleet_vehicle_log_services.py
@@ -10,10 +10,14 @@ class FleetVehicleLogServices(models.Model):
     meeting_count = fields.Integer("# Meetings", compute="_compute_meeting_count")
 
     def _compute_meeting_count(self):
-        meeting_data = self.env["calendar.event"].read_group(
-            [("vehicle_service_id", "in", self.ids)],
-            ["vehicle_service_id"],
-            ["vehicle_service_id"],
+        meeting_data = (
+            self.env["calendar.event"]
+            .sudo()
+            .read_group(
+                [("vehicle_service_id", "in", self.ids)],
+                ["vehicle_service_id"],
+                ["vehicle_service_id"],
+            )
         )
         mapped_data = {
             m["vehicle_service_id"][0]: m["vehicle_service_id_count"]
@@ -36,7 +40,7 @@ class FleetVehicleLogServices(models.Model):
             "default_partner_id": self.user_id.partner_id.id if self.user_id else False,
             "default_partner_ids": partner_ids,
             "default_name": "%s - %s"
-            % (self.vehicle_id.name, self.cost_subtype_id.name),
+            % (self.vehicle_id.name, self.service_type_id.name),
             "search_default_vehicle_service_id": self.id,
         }
         return action
@@ -51,8 +55,7 @@ class FleetVehicleLogServices(models.Model):
             fields.Datetime.context_timestamp(self, meet_date)
         )
         html_time = "<time datetime='{}+00:00'>{}</time>".format(
-            meeting_date,
-            meeting_usertime,
+            meeting_date, meeting_usertime,
         )
         message = _(
             "Meeting scheduled at '%s'<br> Subject: %s <br> Duration: %s hours"
