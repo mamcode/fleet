41b3642816aae82686b231cd62753880a26db2f8
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-03-21 04:52:55 -0300

    [IMP] improvements from 14.0

--- fleet_vehicle_inspection/models/fleet_vehicle_inspection_line.py
+++ fleet_vehicle_inspection/models/fleet_vehicle_inspection_line.py
@@ -6,65 +6,63 @@ from odoo import api, fields, models
 
 class FleetVehicleInspectionLine(models.Model):
 
-    _name = 'fleet.vehicle.inspection.line'
-    _description = 'Fleet Vehicle Inspection Line'
-    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _name = "fleet.vehicle.inspection.line"
+    _description = "Fleet Vehicle Inspection Line"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
 
     READONLY_STATES = {
-        'confirmed': [('readonly', True)],
-        'cancel': [('readonly', True)],
+        "confirmed": [("readonly", True)],
+        "cancel": [("readonly", True)],
     }
 
     inspection_id = fields.Many2one(
-        'fleet.vehicle.inspection',
-        string='Inspection Reference',
+        "fleet.vehicle.inspection",
+        string="Inspection Reference",
         required=True,
-        ondelete='cascade',
-        index=True)
+        ondelete="cascade",
+        index=True,
+    )
 
     inspection_item_id = fields.Many2one(
-        'fleet.vehicle.inspection.item',
-        'Inspection Item',
+        "fleet.vehicle.inspection.item",
+        "Inspection Item",
         required=True,
-        track_visibility="onchange",
+        tracking=True,
         index=True,
-        ondelete='cascade',
+        ondelete="cascade",
         states=READONLY_STATES,
-        copy=True
+        copy=True,
     )
 
     inspection_item_instruction = fields.Text(
-        'Instruction',
-        related='inspection_item_id.instruction')
+        "Instruction", related="inspection_item_id.instruction"
+    )
 
-    result = fields.Selection([
-        ('todo', 'Todo'),
-        ('success', 'Success'),
-        ('failure', 'Failure')
-    ], 'Result', default='todo',
+    result = fields.Selection(
+        [("todo", "Todo"), ("success", "Success"), ("failure", "Failure")],
+        "Result",
+        default="todo",
         readonly=True,
         required=True,
-        copy=False
+        copy=False,
     )
 
     result_description = fields.Char()
 
-    state = fields.Selection([
-        ('draft', 'Draft'),
-        ('confirmed', 'Confirmed'),
-        ('cancel', 'Cancelled'),
-    ], related='inspection_id.state',
-        string='Inspection Status',
+    state = fields.Selection(
+        related="inspection_id.state",
         readonly=True,
-        copy=False,
         store=True,
-        default='draft'
+    )
+
+    inspection_line_image_ids = fields.One2many(
+        "fleet.vehicle.inspection.line.image", "inspection_line_id", string="Images"
     )
 
     @api.multi
     def action_item_success(self):
-        return self.write({'result': 'success'})
+        return self.write({"result": "success"})
 
     @api.multi
     def action_item_failure(self):
-        return self.write({'result': 'failure'})
+        return self.write({"result": "failure"})
