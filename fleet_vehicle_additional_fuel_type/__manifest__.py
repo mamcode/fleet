27174bedfbc1a34c5286aad8711960f860bce22a
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-04-01 00:42:15 -0300

    [ADD] add fleet_vehicle_additional_fuel_type

---
+++ fleet_vehicle_additional_fuel_type/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 - TODAY, Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Fleet Vehicle Additional Fuel Type',
+    'summary': """
+        This module extends the fleet management functionality. It adds other types of fuel to be used by a vehicle.""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'category': 'Human Resources/Fleet',
+    'author': 'Escodoo,Odoo Community Association (OCA)',
+    'maintainers': ['marcelsavegnago'],
+    'images': ['static/description/banner.png'],
+    'website': 'https://github.com/OCA/fleet',
+    'depends': [
+        'fleet',
+    ],
+}
---
+++ fleet_vehicle_additional_fuel_type/readme/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+This module extends the fleet management functionality. It adds other types of fuel to be used by a vehicle.
+
+* Ethanol
+* Flex(Gasoline/Ethanol)
